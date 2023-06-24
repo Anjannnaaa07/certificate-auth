0d5efcffffb15b9de0f206a1c0158946c8a351d8
Anjannnaaa07 <74anjana74@gmail.com>
2023-06-24 09:50:35 +0530

    Firebase working

--- src/pages/certi.jsx
+++ src/pages/certi.jsx
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { storage } from '../firebaseConfig';
-import { ref, getDownloadURL } from "firebase/storage";
-import addToProfile from '../utils/button-add-to-profile';
+import { ref, getDownloadURL } from 'firebase/storage';
+import { getDatabase, ref as dbRef, onValue } from 'firebase/database';
 
 const Certificate = () => {
   const { id } = useParams();
   const [imageUrl, setImageUrl] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [participantName, setParticipantName] = useState('');
+  const [workshopNames, setWorkshopNames] = useState('');
 
   useEffect(() => {
-    const fetchImageUrl = async () => {
+    const fetchData = async () => {
+      const db = getDatabase();
+      const participantRef = dbRef(db, `users/${id}`);
+
+      onValue(participantRef, (snapshot) => {
+        const participantData = snapshot.val();
+        if (participantData && participantData.name) {
+          setParticipantName(participantData.name);
+          fetchWorkshopData(participantData.wid);
+        }
+      });
+
       try {
-        setLoading(true);
         const url = await getDownloadURL(ref(storage, `/${id}.png`));
         setImageUrl(url);
         setLoading(false);
@@ -22,22 +34,34 @@ const Certificate = () => {
       }
     };
 
-    fetchImageUrl();
+    fetchData();
   }, [id]);
+
+  const fetchWorkshopData = async (widArray) => {
+    const db = getDatabase();
+    const workshopsRef = dbRef(db, 'workshops');
+
+    onValue(workshopsRef, (snapshot) => {
+      const workshopsData = snapshot.val();
+      const workshopNames = widArray.map((wid) => workshopsData[wid].wname);
+      setWorkshopNames(workshopNames.join(', '));
+    });
+  };
 
   const handleDownload = () => {
     const link = document.createElement('a');
     link.href = imageUrl;
-    link.download = "certificate";
+    link.download = 'certificate';
     link.click();
   };
 
   return (
-    <div className='cert'>
+    <div className="cert">
       {/* Testing purpose */}
       Now showing post {id}
 
-      <h1 className='cert-header'>Name of Event for which certificate is generated</h1>
+      <h2 className="cert-header">Participant Name: {participantName}</h2>
+      <h2 className="cert-header">Workshop Name(s): {workshopNames}</h2>
       <div className="grid-container">
         <div className="image-container">
           {loading ? (
@@ -48,14 +72,12 @@ const Certificate = () => {
             <div>Not found...</div>
           )}
         </div>
-        <div class="button-container">
+        <div className="button-container">
           <div className="button" onClick={handleDownload}>
             Download Certificate
           </div>
           <div className="button">Share Via LinkedIn</div>
-          <div className="button" onClick={addToProfile}>
-            Add to LinkedIn Profile
-          </div>
+          <div className="button">Add to LinkedIn Profile</div>
           <div className="button">Copy Link</div>
         </div>
       </div>
